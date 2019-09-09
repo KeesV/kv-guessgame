dfc95562ff58a090e52db9f4ee8782c8d2ad836f
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-09-09 14:11:33 +0200

    Process messages

--- led-desk/led-desk.py
+++ led-desk/led-desk.py
@@ -8,8 +8,29 @@ base_topic = os.getenv("MQTT_BASE_TOPIC")
 
 mqttClient = mqtt.Client(client_name)
 
-mqttClient.connect(host_name)
-mqttClient.subscribe(base_topic + "/command")
+
+def blink_start():
+    print("Blinking start")
+
+
+def blink_stop():
+    print("Blinking stop")
+
+
+def blink_wait():
+    print("Blinking wait")
+
+
+def blink_wait_shimmer():
+    print("Blinking wait shimmer")
+
+
+def blink_correct():
+    print("Blinking correct")
+
+
+def blink_incorrect():
+    print("Blinking incorrect")
 
 
 def on_connect(client, userdata, flags, rc):
@@ -27,9 +48,25 @@ def on_message(client, userdata, message):
     print("message qos=", message.qos)
     print("message retain flag=", message.retain)
 
+    if content == "START":
+        blink_start()
+    elif content == "STOP":
+        blink_stop()
+    elif content == "WAIT":
+        blink_wait
+    elif content == "WAIT_SHIMMER":
+        blink_wait_shimmer()
+    elif content == "CORRECT":
+        blink_correct()
+    elif content == "INCORRECT":
+        blink_incorrect()
+
 
 mqttClient.on_connect
 mqttClient.on_subscribe
 mqttClient.on_message = on_message
+
+mqttClient.connect(host_name)
+mqttClient.subscribe(base_topic + "/command")
 
 mqttClient.loop_forever()
